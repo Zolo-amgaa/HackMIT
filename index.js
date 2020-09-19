59debc567cad7de0f96aa663b91a95f94ccc31e1
xamgaa <zoloamg@yahoo.com>
2020-09-19 04:57:41 -0500

    starting to make <4 player feature

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const socket = require('socket.io')
 
-
-var number = 0;
+var players = []; 
+var numberOfPlayers = 0;
 
 //App Setup
 const app = express()
@@ -16,18 +16,47 @@ var server = app.listen(port, () => {
 //Static Files
 app.use(express.static('./website'));
 
+//player check 
+
+
 //Socket Setup
 var io = socket(server);
-
 io.on('connection', function(socket){
+  if(numberOfPlayers < 4) {
+    socket.emit('gameReady', false); 
+  }
+  else {
+    socket.emit("gameReady", true); 
+  }
   console.log('Connection made for id: ',socket.id);
-  number++;
-  console.log(number);
+  let i; 
+  for(i = 0; i<players.length;i++) {
+      if(players[i].id == socket.id) {
+        break; 
+      }
+      
+  }
+  if(i == players.length) {
+      let newPlayer = new Object(); 
+      newPlayer.id = socket.id; 
+      newPlayer.wpm = 0; 
+      players.push(newPlayer); 
+      numberOfPlayers++; 
+  }
+  
+ 
   socket.on('disconnect', () => {
+    numberOfPlayers--;
     console.log('user disconnected');
   });
   socket.on('wpm', (data) => {
-    console.log("cur wpm is " + data); 
+    for(let i = 0; i<players.length;i++) {
+      if(players[i].id == socket.id) {
+        players[i].wpm = data; 
+      }
+      
+  }
+    console.log(players); 
   })
 });
 
