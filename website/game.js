4f46d05cb53c39e33ce0b5130afe9d867effefd7
Aidan Seeberg <aidan.seeberg@gmail.com>
2020-09-19 14:40:18 -0500

    Update game.js

--- website/game.js
+++ website/game.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', init);
 
 // Globals
-let difficulty;
+let difficulty = 'medium';
+
 
 var socket = io.connect();
 
@@ -20,7 +21,8 @@ socket.on('gameReady', ()=> {
 });
 
 socket.on('sendDifficulty', (data) => {
-  difficulty = data; 
+  difficulty = data;
+
 })
 
 
@@ -67,15 +69,16 @@ function startGame() {
   // Show number of seconds in UI
   // Load word from array
   showWord();
+
+  //showWord();
   // Start matching on word input
   wordInput.addEventListener('input', startMatch);
   // Call countdown every second
   setInterval(countup, 1000);
-  // Check game status
-  setInterval(checkStatus, 50);
 
   //Check wpm
   setInterval(updateWpm, 1000);
+
   time = 0;
 }
 
@@ -86,11 +89,11 @@ function startMatch() {
     showWord();
     wordInput.value = '';
     score++;
-
+    console.log(score);
   }
 
   // If score is -1, display 0
-  if (score === -1) {
+  if (score == -1) {
     scoreDisplay.innerHTML = 0;
   } else {
     scoreDisplay.innerHTML = score;
@@ -101,6 +104,7 @@ function updateWpm() {
     wpm = Math.round((score/time)*60);
     wpmDisplay.innerHTML = wpm;
     socket.emit('wpm', wpm);
+
 }
 // Match currentWord to wordInput
 function matchWords() {
@@ -114,9 +118,6 @@ function matchWords() {
   }
 }
 
-function accuracy() {
-
-}
 
 // Pick & show random word
 function showWord() {
@@ -130,6 +131,7 @@ function showWord() {
   if(difficulty == "long"){
     words = longWords;
   }
+  console.log(difficulty);
   // Generate random array index
   const randIndex = Math.floor(Math.random() * words.length);
   // Output random word
@@ -139,6 +141,7 @@ function showWord() {
 // Countdown timer
 function countup() {
   if (countDown == 0) {
+    //eliminate someone
     countDown = 30;
   }
   if (countDown <= 10) {
@@ -151,13 +154,5 @@ function countup() {
   time++;
   // Show time
   timeDisplay.innerHTML = countDown;
-  console.log(wpm);
-}
-
-// Check game status
-function checkStatus() {
-//   if (!isPlaying) {
-//     message.innerHTML = 'Game Over!!!';
-//     score = -1;
-//   }
+  console.log("WPM: " + wpm + "Score: " + score + "\nTime: " + time);
 }
