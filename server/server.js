87c08fd64780949f913a2d895957419797825075
Aidan Seeberg <aidan.seeberg@gmail.com>
2020-09-18 23:52:40 -0500

    Changed names

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ const socket = require('socket.io')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+
+//app.get('/', (req, res) => {
+  //res.send('Hello World!');
+//})
 
 var server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:3000`);
