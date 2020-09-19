109f942fa7c29dfc9e3beb850ccefa5ec23d0741
Aidan Seeberg <aidan.seeberg@gmail.com>
2020-09-19 00:13:55 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,16 +8,12 @@ const app = express()
 const port = 3000
 
 
-//app.get('/', (req, res) => {
-  //res.send('Hello World!');
-//})
-
 var server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:3000`);
 })
 
 //Static Files
-app.use(express.static('../startbootstrap-grayscale-gh-pages'));
+app.use(express.static('../website'));
 
 //Socket Setup
 var io = socket(server);
