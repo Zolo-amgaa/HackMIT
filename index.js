ed57997399217802cf417ab79166c1aba24d27df
Aidan Seeberg <aidan.seeberg@gmail.com>
2020-09-20 01:27:43 -0500

    Bug fixes

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ var numberOfPlayers = 0;
 //Time Management
 var time = 33;
 var elapsed = 0;
-
+var changetime;
 //Socket Setup
 var io = socket(server);
 
 io.on('connection', function(socket) {
 
-  if(inGame || queueOfPlayers.length < 4) {
+
+  if (inGame || queueOfPlayers.length < 4) {
     newPlayer = new Object();
     newPlayer.id = socket.id;
     newPlayer.wpm = 0;
@@ -36,8 +37,7 @@ io.on('connection', function(socket) {
     newPlayer.name = ""
     queueOfPlayers.push(newPlayer);
     //add person to queue
-  }
-  else {
+  } else {
 
   }
   console.log("Player joined with id: " + socket.id + "\nNumber of players in queue: " + queueOfPlayers.length);
@@ -52,35 +52,19 @@ io.on('connection', function(socket) {
   })
 
 
-  if (queueOfPlayers.length >= 4 && !inGame) {
-    for(var i = 0; i<4; i++) {
-      players[i] = queueOfPlayers.shift();
-    }
-    for(var i = 0; i<4; i++) {
-      console.log(players[i].id)
-      io.to(players[i].id).emit('gameReady');
-    }
-    inGame = true;
-    time = 33;
-    setInterval(changeTime, 1000)
-  }
 
 
 
   socket.on('disconnect', () => {
 
-    for (var i = 0; i < players.length; i++)
-    {
-      if (socket.id == players[i].id)
-      {
+    for (var i = 0; i < players.length; i++) {
+      if (socket.id == players[i].id) {
         players.splice(i, 1);
       }
     }
 
-    for (var i = 0; i < queueOfPlayers.length; i++)
-    {
-      if (socket.id == queueOfPlayers[i].id)
-      {
+    for (var i = 0; i < queueOfPlayers.length; i++) {
+      if (socket.id == queueOfPlayers[i].id) {
         queueOfPlayers.splice(i, 1);
       }
     }
@@ -95,6 +79,24 @@ io.on('connection', function(socket) {
       }
     }
   })
+
+
+  setTimeout(function() {
+    if (queueOfPlayers.length >= 4 && !inGame) {
+      for (var i = 0; i < 4; i++) {
+        players[i] = queueOfPlayers.shift();
+      }
+      for (var i = 0; i < 4; i++) {
+        console.log(players[i].id)
+        io.to(players[i].id).emit('gameReady');
+      }
+      inGame = true;
+      time = 33;
+      changetime = setInterval(changeTime, 1000);
+    }
+  }, 250);
+
+
 });
 
 function calculateLevels() {
@@ -108,7 +110,7 @@ function calculateLevels() {
       rank: players[1].rank
     });
   }
-  if (players.length == 3) {
+  else if (players.length == 3) {
     io.to(players[0].id).emit("sendDifficulty", {
       difficulty: "long",
       rank: players[0].rank
@@ -122,23 +124,23 @@ function calculateLevels() {
       rank: players[2].rank
     })
   }
-
-  for (let j = 0; j < players.length; j++) {
-
-    let level = (players[j].rank + 1) / numberOfPlayers;
-    let difficulty = -1;
-
-    if (level <= 0.25)
-      difficulty = "long";
-    else if (level > 0.25 && level <= 0.75)
-      difficulty = "medium";
-    else
-      difficulty = "short";
-
-    io.to(players[j].id).emit("sendDifficulty", {
-      difficulty: difficulty,
-      rank: players[j].rank
-    })
+else if (players.length == 4){
+  io.to(players[0].id).emit("sendDifficulty", {
+    difficulty: "long",
+    rank: players[0].rank
+  })
+  io.to(players[1].id).emit("sendDifficulty", {
+    difficulty: "medium",
+    rank: players[1].rank
+  })
+  io.to(players[2].id).emit("sendDifficulty", {
+    difficulty: "medium",
+    rank: players[2].rank
+  })
+  io.to(players[3].id).emit("sendDifficulty", {
+    difficulty: "short",
+    rank: players[3].rank
+  })
   }
 }
 
@@ -160,11 +162,18 @@ function selectionSort() {
   for (let k = 0; k < len; k++) {
     players[k].rank = k;
   }
+
   calculateLevels();
-  io.emit('leaderboard', players)
+
+  for (var i = 0; i < players.length; i++) {
+    io.to(players[i].id).emit('leaderboard',players);
+  }
 }
 
 function changeTime() {
+
+  selectionSort();
+
   if (time <= 0) {
     eliminateLowest();
     time = 30;
@@ -172,8 +181,11 @@ function changeTime() {
   time--;
   elapsed++;
 
-  for(var i = 0; i<players.length; i++) {
-    io.to(players[i].id).emit('time', {time:time, elapsed:elapsed});
+  for (var i = 0; i < players.length; i++) {
+    io.to(players[i].id).emit('time', {
+      time: time,
+      elapsed: elapsed
+    });
   }
 
   console.log("Array Size: " + players.length + "\nQueue Size: " + queueOfPlayers.length);
@@ -187,19 +199,17 @@ function eliminateLowest() {
 
     selectionSort();
 
-    if(players.length =1)
-    {
+    if (players.length == 1) {
       io.to(players[0].id).emit("end", {
         name: players[0].name,
         wpm: players[0].wpm,
         win: true
       });
-      inGame=false;
+      inGame = false;
       endGame();
       checkReset();
     }
-    if (players.length == 2)
-    {
+    if (players.length == 2) {
       io.to(players[0].id).emit("end", {
         name: players[0].name,
         wpm: players[0].wpm,
@@ -212,11 +222,10 @@ function eliminateLowest() {
       });
 
       console.log("Eliminating " + name + "(" + id + ")");
-      inGame=false;
+      inGame = false;
       endGame();
       checkReset();
-    }
-    else{
+    } else {
       var name = players[players.length - 1].name;
       var wpm = players[players.length - 1].wpm;
       var id = players[players.length - 1].id;
@@ -225,38 +234,37 @@ function eliminateLowest() {
       players.splice(players.length - 1, 1);
 
 
-        io.to(id).emit("end", {
-          name: name,
-          wpm: wpm,
-          win: false
-        });
-        console.log("Eliminating " + name + "(" + id + ")");
-      }
+      io.to(id).emit("end", {
+        name: name,
+        wpm: wpm,
+        win: false
+      });
+      console.log("Eliminating " + name + "(" + id + ")");
     }
-    //collects information of the person being eliminated
-
   }
 
-function endGame()
-{
-  elapsed=0;
-  time=33;
+}
+
+function endGame() {
+  clearInterval(changetime);
+  clearInterval(changetime);
+  elapsed = 0;
+  time = 33;
   players = [];
   console.log("GAME ENDED");
 }
-function checkReset()
-{
-  if (queueOfPlayers.length >= 4)
-  {
-    for(var i = 0; i<4; i++) {
+
+function checkReset() {
+  if (queueOfPlayers.length >= 4) {
+    for (var i = 0; i < 4; i++) {
       players[i] = queueOfPlayers.shift();
     }
-    for(var i = 0; i<4; i++) {
+    for (var i = 0; i < 4; i++) {
       console.log(players[i].id)
       io.to(players[i].id).emit('gameReady');
     }
     inGame = true;
     time = 33;
-    setInterval(changeTime, 1000)
+    changetime = setInterval(changeTime, 1000);
   }
 }
